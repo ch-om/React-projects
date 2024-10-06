8185f942e897162fca1c19c613bde904e25cad04
Welfier <163029702+Welfier@users.noreply.github.com>
2024-10-06 22:19:52 +0530

    changed

--- Project-1/src/components/Hero.jsx
+++ Project-1/src/components/Hero.jsx
@@ -1,33 +1,26 @@
-const HeroSection = () => {
-  return (
-    <main className="hero container">
-      <div className="hero-content">
-        <h1>YOUR FEET DESERVE THE BEST</h1>
-        <p>
-          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
-          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
-          SHOES.
-        </p>
+export default function Hero()
+{
+    return(
+        <main className="hero container">
+         <div className="content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p className="avail2">YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.</p>
 
-        <div className="hero-btn">
-          <button>Shop Now </button>
-          <button className="secondary-btn">Category</button>
-        </div>
+            <div className="button">
+                <button className="btn1">Shop Now</button>
+                <button className="btn2">Category</button>
+            </div>
+            
+            <div>
+                <p className="avail">Also Available on</p>
+            </div>
+            <div className="png">
+                <img src="./images/flipkart.png" alt="" />
+                <img src="./images/amazon.png" alt="" />
+            </div>
+         </div> 
+         <img src="./images/shoe_image.png" alt="" />
 
-        <div className="shopping">
-          <p>Also Available On</p>
-
-          <div className="brand-icons">
-            <img src="/images/amazon.png" alt="amazon-logo" />
-            <img src="/images/flipkart.png" alt="flipkart-logo" />
-          </div>
-        </div>
-      </div>
-      <div className="hero-image">
-        <img src="/images/hero-image.png" alt="hero-image" />
-      </div>
-    </main>
-  );
-};
-
-export default HeroSection;
+        </main>
+    )
+}
